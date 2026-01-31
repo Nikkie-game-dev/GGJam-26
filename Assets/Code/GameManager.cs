211398f8e1fb59664e1477e79cd0ff3d85df9328
Kapnack <unknowrealperson@gmail.com>
2026-01-31 11:23:14 -0300

    Create GameManager.cs
    Add GameManager

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Code.ServiceProvider
+{
+    internal class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ServiceProvider.Instance.AddService<InputManager>(new InputManager());
+        }
+    }
+}
