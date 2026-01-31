ef6487478f9ccf09f25d90b48a5c4cdd71441357
Kapnack <unknowrealperson@gmail.com>
2026-01-31 12:23:32 -0300

    Create Player Movement

---
+++ Assets/Code/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Code.Service;
+using UnityEngine;
+
+namespace Assets.Code.Player
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Collider))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        private float MovementDir => ServiceProvider.Instance.GetService<InputManager>().InputSystem.Player.MovingDir.ReadValue<float>();
+
+        private bool _canJump = false;
+
+        private Rigidbody _rb;
+
+        [SerializeField] private float _jumpForce = 10;
+        [SerializeField] private float _moveForce = 100;
+
+        private bool _isMoving = false;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void Start()
+        {
+            ServiceProvider.Instance.GetService<InputManager>().InputSystem.Player.Jump.started += OnJump;
+            ServiceProvider.Instance.GetService<InputManager>().InputSystem.Player.Move.started += OnMoveStarted;
+            ServiceProvider.Instance.GetService<InputManager>().InputSystem.Player.Move.canceled += OnMoveCanceled;
+        }
+
+        private void Update()
+        {
+            if (_isMoving)
+                OnMove();
+        }
+
+        private void OnMoveStarted(UnityEngine.InputSystem.InputAction.CallbackContext context)
+        {
+            _isMoving = true;
+        }
+
+        private void OnMoveCanceled(UnityEngine.InputSystem.InputAction.CallbackContext context)
+        {
+            _isMoving = false;
+        }
+
+        private void OnMove()
+        {
+            _rb.AddForce((MovementDir > 0 ? Vector3.right : Vector3.left) * Time.deltaTime * _moveForce, ForceMode.Force);
+        }
+
+        private void OnJump(UnityEngine.InputSystem.InputAction.CallbackContext _)
+        {
+            if (_canJump)
+            {
+                _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+                _canJump = false;
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            _canJump = true;
+        }
+    }
+}
