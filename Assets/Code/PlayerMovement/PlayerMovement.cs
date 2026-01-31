b440f6e0a33dd5816b0e184154049c2959b03cfd
Nikkie-Game-Dev <nikkiegallardo@outlook.com.ar>
2026-01-31 15:19:48 -0300

    fix unnecessary imports

--- Assets/Code/PlayerMovement/PlayerMovement.cs
+++ Assets/Code/PlayerMovement/PlayerMovement.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Assets.Code.Service;
-using Systems.LayerClassGenerator;
+﻿using Assets.Code.Service;
 using Systems.TagClassGenerator;
 using UnityEngine;
 
