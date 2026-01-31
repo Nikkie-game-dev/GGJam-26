5133d4de89e0f1d7637bb4b3c066de2393c25969
Kapnack <unknowrealperson@gmail.com>
2026-01-31 14:55:39 -0300

    Fix Request

--- Assets/Code/LifeTimeComponent/LifeTimeComponent.cs
+++ Assets/Code/LifeTimeComponent/LifeTimeComponent.cs
@@ -11,30 +11,22 @@ public delegate void OnLifeTimeEqualsZero();
 public class LifeTimeComponent : MonoBehaviour
 {
     [Header("Properties")]
-    [SerializeField][Tooltip("This is the maximum lifetime the player can get")] 
+    [SerializeField]
+    [Tooltip("This is the maximum lifetime the player can get")]
     private float _maximumLifeTime;
-    [SerializeField][Tooltip("This is the dynmic variable of lifetime, when zero OnDie() ")]  
+    [SerializeField]
+    [Tooltip("This is the dynmic variable of lifetime, when zero OnDie() ")]
     private float _currentLifeTime;
-    [SerializeField][Range(0f, 2f)][Tooltip("This is the time drain multiplier")]
+    [SerializeField]
+    [Range(0f, 2f)]
+    [Tooltip("This is the time drain multiplier")]
     private float _drainFactor = 1;
 
-    [Header("Events")]
-    private OnLifeTimeEqualsZero _onLifeTimeEqualsZero;
-    private CentralizeEventSystem _centralizeEventSystem;
+    private CentralizeEventSystem _centralizeEventSystem => ServiceProvider.Instance.GetService<CentralizeEventSystem>();
+
     private void Start()
     {
         _currentLifeTime = _maximumLifeTime;
-        
-        _centralizeEventSystem = ServiceProvider.Instance.GetService<CentralizeEventSystem>();
-        if(_centralizeEventSystem != null)
-        {
-            _centralizeEventSystem.Register(_onLifeTimeEqualsZero);
-        }
-        else
-        {
-            Debug.LogWarning("centralize event system is null");
-        }
-
     }
     private void Update()
     {
@@ -42,16 +34,14 @@ public class LifeTimeComponent : MonoBehaviour
 
         if (_currentLifeTime <= 0 && _centralizeEventSystem != null)
         {
-            OnLifeTimeEqualsZero delegateInstance = _centralizeEventSystem.Get<OnLifeTimeEqualsZero>();
-            if (delegateInstance != null)
-                delegateInstance.Invoke();
+            _centralizeEventSystem.Get<OnLifeTimeEqualsZero>().Invoke();
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(!other.TryGetComponent<FlowerBase>(out FlowerBase flower)) return;
-        
+        if (!other.TryGetComponent<FlowerBase>(out FlowerBase flower)) return;
+
         AddTime(flower.GetTime());
         flower.Interact(this.gameObject);
     }
@@ -64,7 +54,6 @@ public class LifeTimeComponent : MonoBehaviour
     {
         _drainFactor = Mathf.Clamp(timeAmount, 0, 2);
     }
-
     private void OnDestroy()
     {
         if (_centralizeEventSystem != null)
