9c29200c6b3ad9b67a52c56a003b11db0079d36d
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-02-01 17:33:52 -0300

    Update Scenes
    Remove Test Scenes and Add Persistent Scene

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -2,15 +2,20 @@
 using Code.InputMG;
 using Systems.CentralizeEventSystem;
 using UnityEngine;
+using Code.SceneManagerController;
 
 namespace Code.Manager
 {
     public sealed class GameManager : MonoBehaviour
     {
+        SceneLoader sceneLoader;
+
         private void Awake()
         {
             ServiceProvider.Instance.AddService<InputManager>(new InputManager());
             ServiceProvider.Instance.AddService<CentralizeEventSystem>(new CentralizeEventSystem());
+
+            sceneLoader = new SceneLoader();
         }
     }
 }
